86db9d0ce73c74d8a64a4b5b7d47c262a0075064
Brad Allen <1brad.allen@gmail.com>
2026-05-24 13:01:46 -0500

    Tested out the Check Your Understanding in type-conversion

---
+++ data-and-variables/chapter-examples/type-conversion.js
@@ -0,0 +1,14 @@
+console.log(Number("2345"));
+console.log(typeof Number("2345"));
+console.log(Number(17));
+
+console.log(Number("23bottles"));
+
+console.log(String(17));
+console.log(String(123.45));
+console.log(typeof String(123.45));
+
+console.log(Number('3 3'));
+console.log(Number('three'));
+
+
