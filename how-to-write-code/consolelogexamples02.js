d1174cbb3c2dbace75734c0c1e1bf2f96f566dc5
Brad Allen <1brad.allen@gmail.com>
2026-05-24 12:49:20 -0500

    Used this same coding block to do the exercises in Values and Data Types

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,5 +1,28 @@
+/*
 console.log("Some Programming Languages:");
 
 console.log("Python\nJavaScript\nJava\nC#\nSwift");
 
 console.log("\tPython\nJavaScript\tJava\nC#\nSwift");
+*/
+/*
+console.log(typeof "17");
+console.log(typeof "3.2");
+*/
+
+/*
+console.log (typeof "This is a string");
+console.log(typeof 'And so is this');
+*/
+
+/*
+console.log("Bruce's beard");
+*/
+
+/*
+console.log(42000);
+console.log(42,000);
+*/
+
+console.log(42, 17, 56, 34, 11, 4.35, 32);
+console.log(3.4, "hello", 45);
